69a1914a3466e27d22ea302d366b2eaed32606a1
Montgomery Watts <mjw4633@rit.edu>
2020-08-29 16:48:19 -0400

    Only make one database query for pagination

--- api/routes/artists.ts
+++ api/routes/artists.ts
@@ -3,6 +3,39 @@ import express from 'express';
 import Artist from './artistModel';
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  const pageQuery: number = Number(req.query.page);
+  const page: number = isNaN(pageQuery) ? 1 : pageQuery;
+  const pageSize: number = 20;
+  try {
+    const aggregationPipeline: any[] = [
+      {
+        $facet: {
+          total: [{ $count: 'total' }],
+          artists: [
+            { $skip: (page - 1) * pageSize },
+            { $limit: pageSize },
+            { $project: { name: 1, uri: 1, images: 1 } }
+          ]
+        }
+      }
+    ];
+    if (req.query.name) {
+      aggregationPipeline.unshift({
+        $match: { $text: { $search: req.query.name } }
+      });
+    }
+    const artistsPage = await Artist.aggregate(aggregationPipeline);
+    const payload = {
+      artists: artistsPage[0].artists,
+      numPages: Math.ceil(artistsPage[0].total[0].total / pageSize)
+    };
+    res.json(payload);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
 router.get('/random', (_req, res) => {
   Artist.aggregate(
     [{ $sample: { size: 1 } }, { $limit: 1 }, { $project: { _id: 1 } }],
--- api/routes/genres.ts
+++ api/routes/genres.ts
@@ -9,14 +9,18 @@ router.get('/', async (req, res) => {
   const page: number = isNaN(pageQuery) ? 1 : pageQuery;
   const pageSize: number = 40;
   try {
-    const genres: object[] = await Genre.find()
-      .sort({ _id: 1 })
-      .skip((page - 1) * pageSize)
-      .limit(pageSize);
-    const numGenres: number = await Genre.countDocuments({});
+    const genrePage: any[] = await Genre.aggregate([
+      { $sort: { _id: 1 } },
+      {
+        $facet: {
+          total: [{ $count: 'total' }],
+          genres: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }]
+        }
+      }
+    ]);
     const payload = {
-      genres,
-      numPages: Math.ceil(numGenres / pageSize)
+      genres: genrePage[0].genres,
+      numPages: Math.ceil(genrePage[0].total[0].total / pageSize)
     };
     res.json(payload);
   } catch (err) {
@@ -30,16 +34,22 @@ router.get('/:genre', async (req, res) => {
   const page: number = isNaN(pageQuery) ? 1 : pageQuery;
   const pageSize: number = 20;
   try {
-    const artists = await Artist.aggregate([
+    const artistsPage = await Artist.aggregate([
       { $match: { genres: req.params.genre } },
-      { $skip: (page - 1) * pageSize },
-      { $limit: pageSize },
-      { $project: { name: 1, uri: 1, images: 1 } }
+      {
+        $facet: {
+          total: [{ $count: 'total' }],
+          artists: [
+            { $skip: (page - 1) * pageSize },
+            { $limit: pageSize },
+            { $project: { name: 1, uri: 1, images: 1 } }
+          ]
+        }
+      }
     ]);
-    const genreDoc: any = await Genre.findById(req.params.genre);
     const payload = {
-      artists,
-      numPages: Math.ceil(genreDoc.numArtists / pageSize)
+      artists: artistsPage[0].artists,
+      numPages: Math.ceil(artistsPage[0].total[0].total / pageSize)
     };
     res.json(payload);
   } catch (e) {
